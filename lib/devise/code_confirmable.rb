735b1be1f65f90f519304c782de44241e4e0a36a
Daniel Olivares <danielo@usesammy.com>
2025-06-12 13:30:45 -0600

    chore: Initial commit
    `bundle gem devise-code_confirmable`

---
+++ lib/devise/code_confirmable.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "code_confirmable/version"
+
+module Devise
+  module CodeConfirmable
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/devise/code_confirmable/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Devise
+  module CodeConfirmable
+    VERSION = "0.1.0"
+  end
+end
