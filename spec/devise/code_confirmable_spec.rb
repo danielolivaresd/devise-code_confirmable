e402cebb8f9b460bf440eb7a42e96d5399d4ae6c
Daniel Olivares <danielo@usesammy.com>
2025-06-12 13:44:16 -0600

    chore: Add rubocop plugins

--- spec/devise/code_confirmable_spec.rb
+++ spec/devise/code_confirmable_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe Devise::CodeConfirmable do
   it 'has a version number' do
-    expect(Devise::CodeConfirmable::VERSION).not_to be nil
+    expect(Devise::CodeConfirmable::VERSION).not_to be_nil
   end
 end
